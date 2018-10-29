21458cd701210c70937f85ae031804d8d150ab0e
Taras <tsamoilenko97@gmail.com>
2018-10-29 15:40:37 +0200

    wrote solution to doubleNumbers and sortByWeight

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,10 @@
-/* eslint-disable-next-line */
 export const doubleNum = num => {
-  // your logic here...
+  const str = num.toString();
+  const strFirstHalf = str.slice(0, str.length / 2);
+  const strSecondHalf = str.slice(str.length / 2);
+
+  if (strFirstHalf === strSecondHalf) {
+    return num;
+  }
+  return Number(str) * 2;
 };
--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,12 @@
-/* eslint-disable-next-line */
 export const sortByWeight = arr => {
-  // your logic here...
+  arr.sort((elem1, elem2) => {
+    const result1 = elem1.split('').reduce((sum, current) => {
+      return sum + parseInt(current);
+    }, 0);
+    const result2 = elem2.split('').reduce((sum, current) => {
+      return sum + parseInt(current);
+    }, 0);
+    return result1 - result2;
+  });
+  return arr;
 };
